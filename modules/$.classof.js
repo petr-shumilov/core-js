2c697b49b32ae974941c159650901137521ffa2e
Petr Shumilov <petr.shumilov@sap.com>
2019-09-16 07:43:13 -0700

    Fix type inference in IE11

--- modules/$.classof.js
+++ modules/$.classof.js
@@ -6,7 +6,18 @@ var cof = require('./$.cof')
 
 module.exports = function(it){
   var O, T, B;
+  
+  var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+  var isObject = function (it) {
+    return (typeof it === 'undefined' ? 'undefined' : _typeof(it)) === 'object' ? it !== null : typeof it === 'function';
+  };
+
   return it === undefined ? 'Undefined' : it === null ? 'Null'
+   // IE 11 in some cases
+    : typeof it.getTime === 'function' ? 'Date' 
+    // IE 11 in some cases
+    : isObject(it) == true ? 'Object'
     // @@toStringTag case
     : typeof (T = (O = Object(it))[TAG]) == 'string' ? T
     // builtinTag case
